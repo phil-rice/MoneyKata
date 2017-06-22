1e87dda2da09b6e7fa71cc08575d7ccb32dce024
Phil <phil.rice@iee.org>
2017-06-22 14:06:57 +0200

    Tidy up

--- src/main/scala/org/money/Money.scala
+++ src/main/scala/org/money/Money.scala
@@ -2,13 +2,6 @@ package org.money
 
 import utils._
 
-object MoneyLanguage {
-  implicit class MoneyPimper[M](m: M)(implicit money: Money[M]) {
-    def add (two: M) = money.add(m, two)
-    def split ( ways: Int) = money.split(m, ways)
-  }
-}
-
 trait Money[M] {
   def add(one: M, two: M): M
 
@@ -16,7 +9,19 @@ trait Money[M] {
 }
 
 class SimpleMoney[M](implicit monoid: Monoid[M], splitable: Splitable[M]) extends Money[M] {
-    override def add(one: M, two: M): M = monoid.add(one, two)
+  override def add(one: M, two: M): M = monoid.add(one, two)
 
-    override def split(s: M, n: Int): AmountAndRemainder[M] = splitable.split(s, n)
+  override def split(s: M, n: Int): AmountAndRemainder[M] = splitable.split(s, n)
 }
+
+object MoneyLanguage {
+
+  implicit class MoneyPimper[M](m: M)(implicit money: Money[M]) {
+    def add(two: M) = money.add(m, two)
+
+    def split(ways: Int) = money.split(m, ways)
+  }
+
+}
+
+
